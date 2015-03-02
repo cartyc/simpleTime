1e5e69ed1b626f55454594f0e54d4bc0abfa2ada
Chris Carty <cartyc@users.noreply.github.com>
2015-03-01 21:40:55 -0500

    Added custom font

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 static Window *s_main_window;
 
 
-
+static GFont *s_time_font;
 //for for the main text
 static TextLayer *s_time_layer;
 
@@ -33,11 +33,13 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed){
 //Load the window
 static void main_window_load(Window *window){
   //Create time TextLayer
-  s_time_layer = text_layer_create(GRect(0,55,144,50));
+  s_time_layer = text_layer_create(GRect(0,5,144,50));
   
-  text_layer_set_background_color(s_time_layer, GColorClear);
-  
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_background_color(s_time_layer, GColorClear);  
+  text_layer_set_text_color(s_time_layer, GColorBlack);
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_LATO_BOLD_42));
+
+  text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer,GTextAlignmentCenter);
   
   layer_add_child(window_get_root_layer(window),text_layer_get_layer(s_time_layer));
