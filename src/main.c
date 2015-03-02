f4f9958ae3e69e09b4f211f4c6c7f49a24e898b4
Chris Carty <cartyc@users.noreply.github.com>
2015-03-01 20:49:28 -0500

    First Commit

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include <pebble.h>
+
+  
+//This Initializes the watch face
+//Everything here has to have an deinit in the deinit()
+static void init(){
+  
+}
+
+static void deinit(){
+  
+}
+  
+int main void(){
+  
+}
